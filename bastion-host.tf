b51c093826e0ac5bdd6ca944c002adf1eee413dc
blndev <blndev@users.noreply.github.com>
2020-02-27 11:58:34 +0100

    upgrade to azure rm V2

--- bastion-host.tf
+++ bastion-host.tf
@@ -50,7 +50,7 @@ resource "azurerm_network_interface" "bastion-nic" {
     resource_group_name = azurerm_resource_group.rg.name
     location            = var.location
     tags                = local.default_tags
-    network_security_group_id = azurerm_network_security_group.network-security-bastion.id
+#    network_security_group_id = azurerm_network_security_group.network-security-bastion.id
 
     ip_configuration {
         name                          = "${local.deploymentname}-nic-bastion-ip"
